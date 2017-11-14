a78d631b0d63e4797f4115c61fdd2204d8378faa
Stan Drapkin <sdrapkin@sdprime.com>
2017-11-14 09:47:05 -0500

    Making CloneBytes() extension on byte[] public.

--- Extensions/ByteArrayExtensions.cs
+++ Extensions/ByteArrayExtensions.cs
@@ -2,22 +2,22 @@
 
 namespace SecurityDriven.Inferno.Extensions
 {
-	internal static class ByteArrayExtensions
+	public static class ByteArrayExtensions
 	{
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		internal static byte[] CloneBytes(this byte[] bytes)
+		public static byte[] CloneBytes(this byte[] bytes)
 		{
 			var clone = new byte[bytes.Length];
 			Utils.BlockCopy(bytes, 0, clone, 0, bytes.Length);
 			return clone;
-		}//Clone()
+		}//CloneBytes()
 
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		internal static byte[] CloneBytes(this byte[] bytes, int offset, int count)
+		public static byte[] CloneBytes(this byte[] bytes, int offset, int count)
 		{
 			var clone = new byte[count];
 			Utils.BlockCopy(bytes, offset, clone, 0, count);
 			return clone;
-		}//Clone()
+		}//CloneBytes()
 	}//class ByteArrayExtensions
 }//ns
