c699cb875e8401a4febada1a98cc84e8304f2498
Stan Drapkin <sdrapkin@sdprime.com>
2020-02-19 15:26:05 -0500

    Performance improvements.

--- Cipher/AesCtrCryptoTransform.cs
+++ Cipher/AesCtrCryptoTransform.cs
@@ -1,11 +1,10 @@
 ﻿using System;
-using System.Security.Cryptography;
 using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
 
 namespace SecurityDriven.Inferno.Cipher
 {
-	using Extensions;
-
 	internal static class AesConstants
 	{
 		public const int AES_BLOCK_SIZE = 16;
@@ -32,45 +31,24 @@ namespace SecurityDriven.Inferno.Cipher
 		public AesCtrCryptoTransform(byte[] key, ArraySegment<byte> counterBufferSegment, Func<Aes> aesFactory = null)
 		{
 			if (counterBufferSegment.Count != AesConstants.AES_BLOCK_SIZE)
-				throw new ArgumentException("counterBufferSegment.Count must be " + AesConstants.STR_AES_BLOCK_SIZE + ".");
+				ThrowNewArgumentException("counterBufferSegment.Count must be " + AesConstants.STR_AES_BLOCK_SIZE + ".");
 
-			var aes = aesFactory == null ? AesFactories.Aes() : aesFactory();
+			var aes = this.aes = aesFactory == null ? AesFactories.Aes() : aesFactory();
+			(aes.Mode, aes.Padding) = (CipherMode.ECB, PaddingMode.None);
 
-			aes.Mode = CipherMode.ECB;
-			aes.Padding = PaddingMode.None;
-
-			var counterBufferSegmentArray = counterBufferSegment.Array;
-			var counterBufferSegmentOffset = counterBufferSegment.Offset;
-
-			byte[] counterBuffer_KeyStreamBuffer = this.counterBuffer_KeyStreamBuffer; // looks dumb, but local-access is faster than field-access
-
+			(var counterBufferSegmentArray, var counterBufferSegmentOffset) = (counterBufferSegment.Array, counterBufferSegment.Offset);
 			System.Diagnostics.Debug.Assert(AesConstants.AES_BLOCK_SIZE == 16);
 
-			//Utils.BlockCopy(counterBufferSegment.Array, counterBufferSegment.Offset, counterBuffer_KeyStreamBuffer, 0, AesConstants.AES_BLOCK_SIZE);
-			counterBuffer_KeyStreamBuffer[00] = counterBufferSegmentArray[counterBufferSegmentOffset + 00];
-			counterBuffer_KeyStreamBuffer[01] = counterBufferSegmentArray[counterBufferSegmentOffset + 01];
-			counterBuffer_KeyStreamBuffer[02] = counterBufferSegmentArray[counterBufferSegmentOffset + 02];
-			counterBuffer_KeyStreamBuffer[03] = counterBufferSegmentArray[counterBufferSegmentOffset + 03];
-			counterBuffer_KeyStreamBuffer[04] = counterBufferSegmentArray[counterBufferSegmentOffset + 04];
-			counterBuffer_KeyStreamBuffer[05] = counterBufferSegmentArray[counterBufferSegmentOffset + 05];
-			counterBuffer_KeyStreamBuffer[06] = counterBufferSegmentArray[counterBufferSegmentOffset + 06];
-			counterBuffer_KeyStreamBuffer[07] = counterBufferSegmentArray[counterBufferSegmentOffset + 07];
+			(Unsafe.As<byte, ulong>(ref this.counterBuffer_KeyStreamBuffer[0]), this.counterStruct.UlongValue) =
+				Unsafe.As<byte, (ulong, ulong)>(ref counterBufferSegmentArray[counterBufferSegmentOffset]);
 
-			this.counterStruct = new Utils.LongStruct
-			{
-				B8 = counterBufferSegmentArray[counterBufferSegmentOffset + 08],
-				B7 = counterBufferSegmentArray[counterBufferSegmentOffset + 09],
-				B6 = counterBufferSegmentArray[counterBufferSegmentOffset + 10],
-				B5 = counterBufferSegmentArray[counterBufferSegmentOffset + 11],
-				B4 = counterBufferSegmentArray[counterBufferSegmentOffset + 12],
-				B3 = counterBufferSegmentArray[counterBufferSegmentOffset + 13],
-				B2 = counterBufferSegmentArray[counterBufferSegmentOffset + 14],
-				B1 = counterBufferSegmentArray[counterBufferSegmentOffset + 15]
-			};
+			if (BitConverter.IsLittleEndian)
+				this.counterStruct.UlongValue = Utils.ReverseEndianness(this.counterStruct.UlongValue);
 
 			this.cryptoTransform = aes.CreateEncryptor(rgbKey: key, rgbIV: null);
-			this.aes = aes;
 		}// ctor
+
+		static int VECTOR_LENGTH = Vector<byte>.Count;
 
 		#region public
 		public int TransformBlock(byte[] inputBuffer, int inputOffset, int inputCount, byte[] outputBuffer, int outputOffset)
@@ -98,60 +76,61 @@ namespace SecurityDriven.Inferno.Cipher
 			int fullBlockSize = (remainingInputCount >> 4) << 4;
 			int partialBlockSize = remainingInputCount - fullBlockSize;
 
-			var _counterStruct = this.counterStruct;
+			ref var _counterStructRef = ref this.counterStruct;
 
 			// process full blocks, if any
 			if (fullBlockSize > 0)
 			{
-				byte counterBuffer_00 = counterBuffer_KeyStreamBuffer[00];
-				byte counterBuffer_01 = counterBuffer_KeyStreamBuffer[01];
-				byte counterBuffer_02 = counterBuffer_KeyStreamBuffer[02];
-				byte counterBuffer_03 = counterBuffer_KeyStreamBuffer[03];
-				byte counterBuffer_04 = counterBuffer_KeyStreamBuffer[04];
-				byte counterBuffer_05 = counterBuffer_KeyStreamBuffer[05];
-				byte counterBuffer_06 = counterBuffer_KeyStreamBuffer[06];
-				byte counterBuffer_07 = counterBuffer_KeyStreamBuffer[07];
-
-				unchecked
+				ref ulong counterHalf1 = ref Unsafe.As<byte, ulong>(ref counterBuffer_KeyStreamBuffer[0]);
+				if (BitConverter.IsLittleEndian)
 				{
 					for (i = outputOffset, /* reusing j as iMax */ j = outputOffset + fullBlockSize; i < j; i += AesConstants.AES_BLOCK_SIZE)
-					{
-						outputBuffer[i + 00] = counterBuffer_00;
-						outputBuffer[i + 01] = counterBuffer_01;
-						outputBuffer[i + 02] = counterBuffer_02;
-						outputBuffer[i + 03] = counterBuffer_03;
-						outputBuffer[i + 04] = counterBuffer_04;
-						outputBuffer[i + 05] = counterBuffer_05;
-						outputBuffer[i + 06] = counterBuffer_06;
-						outputBuffer[i + 07] = counterBuffer_07;
-
-						outputBuffer[i + 08] = _counterStruct.B8;
-						outputBuffer[i + 09] = _counterStruct.B7;
-						outputBuffer[i + 10] = _counterStruct.B6;
-						outputBuffer[i + 11] = _counterStruct.B5;
-						outputBuffer[i + 12] = _counterStruct.B4;
-						outputBuffer[i + 13] = _counterStruct.B3;
-						outputBuffer[i + 14] = _counterStruct.B2;
-						outputBuffer[i + 15] = _counterStruct.B1;
-
-						{ ++_counterStruct.UlongValue; };
-					}//for
+						Unsafe.As<byte, (ulong, ulong)>(ref outputBuffer[i]) = (counterHalf1, Utils.ReverseEndianness(_counterStructRef.UlongValue++));
 				}
+				else
+				{
+					for (i = outputOffset, /* reusing j as iMax */ j = outputOffset + fullBlockSize; i < j; i += AesConstants.AES_BLOCK_SIZE)
+						Unsafe.As<byte, (ulong, ulong)>(ref outputBuffer[i]) = (counterHalf1, _counterStructRef.UlongValue++);
+				}
+
 				fullBlockSize = this.cryptoTransform.TransformBlock(outputBuffer, outputOffset, fullBlockSize, outputBuffer, outputOffset);
 				i = 0;
 
 				const bool VECTORIZE = true;
 				if (VECTORIZE)
 				{   // vectorized xor
-					int vectorLength = Vector<byte>.Count, vectorLimit = fullBlockSize - vectorLength;
-					for (; i <= vectorLimit; i += vectorLength)
-					{
-						var destVector = new Vector<byte>(outputBuffer, outputOffset + i);
-						var leftVector = new Vector<byte>(inputBuffer, inputOffset + i);
-						(destVector ^ leftVector).CopyTo(outputBuffer, outputOffset + i);
-					}
-				}
+					int vectorLength_x2 = VECTOR_LENGTH << 1;
+					int vectorLength_x4 = VECTOR_LENGTH << 2;
 
+					int wideVectorLength;
+					int vectorLimit;
+
+					wideVectorLength = vectorLength_x4;
+					vectorLimit = fullBlockSize - wideVectorLength;
+
+					for (; i <= vectorLimit; i += wideVectorLength)
+					{
+						ref var destVectors = ref Unsafe.As<byte, (Vector<byte>, Vector<byte>, Vector<byte>, Vector<byte>)>(ref outputBuffer[outputOffset + i]);
+						ref var leftVectors = ref Unsafe.As<byte, (Vector<byte>, Vector<byte>, Vector<byte>, Vector<byte>)>(ref inputBuffer[inputOffset + i]);
+
+						destVectors.Item4 ^= leftVectors.Item4;
+						destVectors.Item3 ^= leftVectors.Item3;
+						destVectors.Item2 ^= leftVectors.Item2;
+						destVectors.Item1 ^= leftVectors.Item1;
+					}
+
+					wideVectorLength = vectorLength_x2;
+					vectorLimit = fullBlockSize - wideVectorLength;
+
+					for (; i <= vectorLimit; i += wideVectorLength)
+					{
+						ref var destVectors = ref Unsafe.As<byte, (Vector<byte>, Vector<byte>)>(ref outputBuffer[outputOffset + i]);
+						ref var leftVectors = ref Unsafe.As<byte, (Vector<byte>, Vector<byte>)>(ref inputBuffer[inputOffset + i]);
+
+						destVectors.Item2 ^= leftVectors.Item2;
+						destVectors.Item1 ^= leftVectors.Item1;
+					}
+				}//if (VECTORIZE)
 				for (; i < fullBlockSize; ++i) outputBuffer[outputOffset + i] ^= inputBuffer[inputOffset + i];
 			}// if fullBlockSize > 0
 
@@ -161,16 +140,9 @@ namespace SecurityDriven.Inferno.Cipher
 				inputOffset += fullBlockSize;
 				outputOffset += fullBlockSize;
 
-				counterBuffer_KeyStreamBuffer[08] = _counterStruct.B8;
-				counterBuffer_KeyStreamBuffer[09] = _counterStruct.B7;
-				counterBuffer_KeyStreamBuffer[10] = _counterStruct.B6;
-				counterBuffer_KeyStreamBuffer[11] = _counterStruct.B5;
-				counterBuffer_KeyStreamBuffer[12] = _counterStruct.B4;
-				counterBuffer_KeyStreamBuffer[13] = _counterStruct.B3;
-				counterBuffer_KeyStreamBuffer[14] = _counterStruct.B2;
-				counterBuffer_KeyStreamBuffer[15] = _counterStruct.B1;
-
-				{ ++_counterStruct.UlongValue; };
+				if (BitConverter.IsLittleEndian)
+				{ /**/ Unsafe.As<byte, ulong>(ref counterBuffer_KeyStreamBuffer[8]) = Utils.ReverseEndianness(_counterStructRef.UlongValue++); }
+				else { Unsafe.As<byte, ulong>(ref counterBuffer_KeyStreamBuffer[8]) = _counterStructRef.UlongValue++; }
 
 				this.cryptoTransform.TransformBlock(counterBuffer_KeyStreamBuffer, 0, AesConstants.AES_BLOCK_SIZE, counterBuffer_KeyStreamBuffer, AesConstants.AES_BLOCK_SIZE);
 
@@ -178,13 +150,12 @@ namespace SecurityDriven.Inferno.Cipher
 				this.keyStreamBytesRemaining = AesConstants.AES_BLOCK_SIZE - partialBlockSize;
 			}//if partialBlockSize > 0
 
-			this.counterStruct = _counterStruct;
 			return inputCount;
 		}// TransformBlock()
 
 		public byte[] TransformFinalBlock(byte[] inputBuffer, int inputOffset, int inputCount)
 		{
-			byte[] outputBuffer = (inputCount == 0) ? _emptyByteArray : new byte[inputCount];
+			byte[] outputBuffer = (inputCount == 0) ? Array.Empty<byte>() : new byte[inputCount];
 			this.TransformBlock(inputBuffer, inputOffset, inputCount, outputBuffer, 0);
 			this.Dispose();
 			return outputBuffer;
@@ -205,13 +176,13 @@ namespace SecurityDriven.Inferno.Cipher
 				finally
 				{
 					var counterBuffer_KeyStreamBuffer = this.counterBuffer_KeyStreamBuffer;
-					unchecked { for (int i = AesConstants.AES_BLOCK_SIZE; i < AesConstants.AES_BLOCK_SIZE * 2; ++i) counterBuffer_KeyStreamBuffer[i] = 0; }
+					Unsafe.InitBlock(ref counterBuffer_KeyStreamBuffer[AesConstants.AES_BLOCK_SIZE], 0, (uint)AesConstants.AES_BLOCK_SIZE);
 					this.aes = null;
 				}
 			}// if aes is not null
 		}// Dispose()
 		#endregion
 
-		static readonly byte[] _emptyByteArray = Array.Empty<byte>();
+		static void ThrowNewArgumentException(string message) => throw new ArgumentException(message);
 	}// class AesCtrCryptoTransform
 }//ns
