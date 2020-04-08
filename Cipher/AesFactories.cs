6174f0a5e0e51c74574a5a670e0e776c14359b4c
Stan Drapkin <sdrapkin@sdprime.com>
2020-04-07 22:35:06 -0400

    Bug fix. (Aes factory was throwing on Linux)

--- Cipher/AesFactories.cs
+++ Cipher/AesFactories.cs
@@ -6,7 +6,9 @@ namespace SecurityDriven.Inferno.Cipher
 	public static class AesFactories
 	{
 		internal static readonly Func<Aes> ManagedAes = () => new AesManaged();
-		internal static readonly Func<Aes> FipsAes = () => new AesCng();// new AesCryptoServiceProvider();
+		internal static readonly Func<Aes> FipsAes = Environment.OSVersion.Platform == PlatformID.Win32NT ?
+			(Func<Aes>)(() => new AesCng()) :		// Windows
+			() => new AesCryptoServiceProvider();	// non-Windows
 
 		public static readonly Func<Aes> Aes = Utils.AllowOnlyFipsAlgorithms ? FipsAes : ManagedAes;
 	}//class AesFactories
