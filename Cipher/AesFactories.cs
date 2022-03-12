d805590369b0276a4e731f6b572bf748d131a292
Konstantin S <3002068+HavenDV@users.noreply.github.com>
2022-03-12 18:38:11 +1000

    feat: Added net5/net6 targets. Added SupportedOSPlatform attrubutes for windows-specific code.

--- Cipher/AesFactories.cs
+++ Cipher/AesFactories.cs
@@ -7,8 +7,10 @@ namespace SecurityDriven.Inferno.Cipher
 	{
 		internal static readonly Func<Aes> ManagedAes = () => new AesManaged();
 		internal static readonly Func<Aes> FipsAes = Environment.OSVersion.Platform == PlatformID.Win32NT ?
-			(Func<Aes>)(() => new AesCng()) :		// Windows
-			() => new AesCryptoServiceProvider();	// non-Windows
+#pragma warning disable CA1416 // Validate platform compatibility
+            (Func<Aes>)(() => new AesCng()) :       // Windows
+#pragma warning restore CA1416 // Validate platform compatibility
+            () => new AesCryptoServiceProvider();	// non-Windows
 
 		public static readonly Func<Aes> Aes = Utils.AllowOnlyFipsAlgorithms ? FipsAes : ManagedAes;
 	}//class AesFactories
--- Extensions/CngKeyExtensions.cs
+++ Extensions/CngKeyExtensions.cs
@@ -6,29 +6,72 @@ namespace SecurityDriven.Inferno.Extensions
 {
 	public static class CngKeyExtensions
 	{
+#if NET5_0_OR_GREATER
+		[System.Runtime.Versioning.SupportedOSPlatform("windows")]
+#elif NETSTANDARD2_0_OR_GREATER || NET461_OR_GREATER || NETCOREAPP3_1
+#else
+#error Target Framework is not supported
+#endif
 		static readonly CngKeyCreationParameters cngKeyCreationParameters = new CngKeyCreationParameters { ExportPolicy = CngExportPolicies.AllowPlaintextExport };
+
+#if NET5_0_OR_GREATER
+		[System.Runtime.Versioning.SupportedOSPlatform("windows")]
+#elif NETSTANDARD2_0_OR_GREATER || NET461_OR_GREATER || NETCOREAPP3_1
+#else
+#error Target Framework is not supported
+#endif
 		static readonly CngProperty exportPolicy_AllowPlaintextExport = new CngProperty("Export Policy", BitConverter.GetBytes((int)CngExportPolicies.AllowPlaintextExport), CngPropertyOptions.None);
 
+#if NET5_0_OR_GREATER
+		[System.Runtime.Versioning.SupportedOSPlatform("windows")]
+#elif NETSTANDARD2_0_OR_GREATER || NET461_OR_GREATER || NETCOREAPP3_1
+#else
+#error Target Framework is not supported
+#endif
 		public static CngKey CreateNewDhmKey(string name = null)
 		{
 			return CngKey.Create(CngAlgorithm.ECDiffieHellmanP384, name, cngKeyCreationParameters);
 		}
 
+#if NET5_0_OR_GREATER
+		[System.Runtime.Versioning.SupportedOSPlatform("windows")]
+#elif NETSTANDARD2_0_OR_GREATER || NET461_OR_GREATER || NETCOREAPP3_1
+#else
+#error Target Framework is not supported
+#endif
 		public static CngKey CreateNewDsaKey(string name = null)
 		{
 			return CngKey.Create(CngAlgorithm.ECDsaP384, name, cngKeyCreationParameters);
 		}
 
+#if NET5_0_OR_GREATER
+		[System.Runtime.Versioning.SupportedOSPlatform("windows")]
+#elif NETSTANDARD2_0_OR_GREATER || NET461_OR_GREATER || NETCOREAPP3_1
+#else
+#error Target Framework is not supported
+#endif
 		public static byte[] GetPrivateBlob(this CngKey key)
 		{
 			return key.Export(CngKeyBlobFormat.EccPrivateBlob);
 		}
 
+#if NET5_0_OR_GREATER
+		[System.Runtime.Versioning.SupportedOSPlatform("windows")]
+#elif NETSTANDARD2_0_OR_GREATER || NET461_OR_GREATER || NETCOREAPP3_1
+#else
+#error Target Framework is not supported
+#endif
 		public static byte[] GetPublicBlob(this CngKey key)
 		{
 			return key.Export(CngKeyBlobFormat.EccPublicBlob);
 		}
 
+#if NET5_0_OR_GREATER
+		[System.Runtime.Versioning.SupportedOSPlatform("windows")]
+#elif NETSTANDARD2_0_OR_GREATER || NET461_OR_GREATER || NETCOREAPP3_1
+#else
+#error Target Framework is not supported
+#endif
 		public static CngKey ToPrivateKeyFromBlob(this byte[] privateBlob)
 		{
 			var key = CngKey.Import(privateBlob, CngKeyBlobFormat.EccPrivateBlob);
@@ -36,6 +79,12 @@ namespace SecurityDriven.Inferno.Extensions
 			return key;
 		}
 
+#if NET5_0_OR_GREATER
+		[System.Runtime.Versioning.SupportedOSPlatform("windows")]
+#elif NETSTANDARD2_0_OR_GREATER || NET461_OR_GREATER || NETCOREAPP3_1
+#else
+#error Target Framework is not supported
+#endif
 		public static CngKey ToPublicKeyFromBlob(this byte[] publicBlob)
 		{
 			return CngKey.Import(publicBlob, CngKeyBlobFormat.EccPublicBlob);
@@ -44,9 +93,15 @@ namespace SecurityDriven.Inferno.Extensions
 		/// <summary>
 		/// Both parties are static and authenticated.
 		/// </summary>
+#if NET5_0_OR_GREATER
+		[System.Runtime.Versioning.SupportedOSPlatform("windows")]
+#elif NETSTANDARD2_0_OR_GREATER || NET461_OR_GREATER || NETCOREAPP3_1
+#else
+#error Target Framework is not supported
+#endif
 		public static byte[] GetSharedDhmSecret(this CngKey privateDhmKey, CngKey publicDhmKey, byte[] contextAppend = null, byte[] contextPrepend = null)
 		{
-#if (NET462 || NETCOREAPP3_1)
+#if (NET462 || NETCOREAPP3_1 || NET5_0 || NET6_0)
 			using (var ecdh = new ECDiffieHellmanCng(privateDhmKey) { HashAlgorithm = CngAlgorithm.Sha384, SecretAppend = contextAppend, SecretPrepend = contextPrepend })
 				return ecdh.DeriveKeyMaterial(publicDhmKey);
 #elif NETSTANDARD2_0
@@ -60,6 +115,12 @@ namespace SecurityDriven.Inferno.Extensions
 		/// Sender is anonymous and keyless.
 		/// Receiver is static and authenticated.
 		/// </summary>
+#if NET5_0_OR_GREATER
+		[System.Runtime.Versioning.SupportedOSPlatform("windows")]
+#elif NETSTANDARD2_0_OR_GREATER || NET461_OR_GREATER || NETCOREAPP3_1
+#else
+#error Target Framework is not supported
+#endif
 		public static SharedEphemeralBundle GetSharedEphemeralDhmSecret(this CngKey receiverDhmPublicKey, byte[] contextAppend = null, byte[] contextPrepend = null)
 		{
 			using (var sender = CreateNewDhmKey())
